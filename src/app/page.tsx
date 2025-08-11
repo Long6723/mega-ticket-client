dd448f2f432f9163e5feab0ed2deb95e143c735f
Long <long06072003@gmai.com>
2025-08-11 17:35:17 +0700

    feat: homepages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,57 @@
 "use client";
 import Footer from "@/components/layout/footer/footer";
 import Header from "@/components/layout/header/header";
+import { Container, Grid } from "@mui/material";
 
 export default function Page() {
+  const data = [
+    {
+      link: "/",
+      img: "https://chieuphimquocgia.com.vn/_next/image?url=https%3A%2F%2Fapi.chieuphimquocgia.com.vn%2FContent%2FImages%2F0018864_0.jpg&w=256&q=75",
+      date: "11/07/2025",
+      name: "SUPERMAN",
+    },
+    {
+      link: "/",
+      img: "https://chieuphimquocgia.com.vn/_next/image?url=https%3A%2F%2Fapi.chieuphimquocgia.com.vn%2FContent%2FImages%2F0018758_0.jpg&w=384&q=75",
+      date: "13/06/2025",
+      name: "BÍ KÍP LUYỆN RỒNG",
+    },
+    {
+      link: "/",
+      img: "https://chieuphimquocgia.com.vn/_next/image?url=https%3A%2F%2Fapi.chieuphimquocgia.com.vn%2FContent%2FImages%2F0018733_0.jpg&w=384&q=75",
+      date: "04/07/2025",
+      name: "THẾ GIỚI KHỦNG LONG: TÁI SINH",
+    },
+    {
+      link: "/",
+      img: "https://chieuphimquocgia.com.vn/_next/image?url=https%3A%2F%2Fapi.chieuphimquocgia.com.vn%2FContent%2FImages%2F0018730_0.jpg&w=256&q=75",
+      date: "18/07/2025",
+      name: "PHIM XÌ TRUM-P ( Lồng tiếng )",
+    },
+    {
+      link: "/",
+      img: "https://chieuphimquocgia.com.vn/_next/image?url=https%3A%2F%2Fapi.chieuphimquocgia.com.vn%2FContent%2FImages%2F0018978_0.jpg&w=256&q=75",
+      date: "08/08/2025",
+      name: "CHỐT ĐƠN",
+    },
+    {
+      link: "/",
+      img: "https://chieuphimquocgia.com.vn/_next/image?url=https%3A%2F%2Fapi.chieuphimquocgia.com.vn%2FContent%2FImages%2F0018625_0.jpg&w=256&q=75",
+      date: "25/07/2025",
+      name: "BỘ TỨ SIÊU ĐẲNG: BƯỚC ĐI ĐẦU TIÊN",
+    },
+    {
+      link: "/",
+      img: "https://chieuphimquocgia.com.vn/_next/image?url=http%3A%2F%2Fapiv2.chieuphimquocgia.com.vn%2FContent%2FImages%2F0018696_0.jpg&w=1920&q=75",
+      date: "01/08/2025",
+      name: "MANG MẸ ĐI BỎ",
+    },
+  ];
   return (
-    <div className="bg-[#10141b] text-white">
+    <div className="bg-[#000] text-white">
       <Header />
-      <div className="w-full">
+      <div className="w-full bg-[#10141b] pb-20">
         <a href="/" className="block">
           <img
             src="https://chieuphimquocgia.com.vn/_next/image?url=http%3A%2F%2Fapiv2.chieuphimquocgia.com.vn%2FContent%2FImages%2FBanner%2F0018969.jpg&w=1920&q=75"
@@ -14,7 +59,55 @@ export default function Page() {
             className="w-full h-auto object-cover"
           />
         </a>
+
+        <Container>
+          <h1 className=" mt-10 mb-5 font-bold text-2xl">Phim đang chiếu</h1>
+          <Grid container spacing={2}>
+            {data.map((item, index) => {
+              return (
+                <Grid size={{ xs: 6, sm: 4, md: 3, lg: 2 }} key={index}>
+                  <div className="overflow-hidden">
+                    <a href={item.link}>
+                      <img
+                        src={item.img}
+                        alt=""
+                        className="transition-transform duration-300 hover:scale-105 w-full h-[264px] "
+                      />
+                      <p className="ml-5 my-2 text-sm text-gray-400 ">
+                        {item.date}
+                      </p>
+                      <h2 className="font-bold">{item.name}</h2>
+                    </a>
+                  </div>
+                </Grid>
+              );
+            })}
+          </Grid>
+          <h1 className=" mt-10 mb-5  font-bold text-2xl">Phim sắp chiếu</h1>
+          <Grid container spacing={2}>
+            {data.map((item, index) => {
+              return (
+                <Grid size={{ xs: 6, sm: 4, md: 3, lg: 2 }} key={index}>
+                  <div className="overflow-hidden">
+                    <a href={item.link}>
+                      <img
+                        src={item.img}
+                        alt=""
+                        className="transition-transform duration-300 hover:scale-105 w-full h-[264px] "
+                      />
+                      <p className="ml-5 my-2 text-sm text-gray-400 ">
+                        {item.date}
+                      </p>
+                      <h2 className="font-bold">{item.name}</h2>
+                    </a>
+                  </div>
+                </Grid>
+              );
+            })}
+          </Grid>
+        </Container>
       </div>
+
       <Footer />
     </div>
   );
--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -1,74 +1,170 @@
 import MyButton from "@/components/ui/button";
 import "./header.scss";
+import { Container, Menu, MenuItem } from "@mui/material";
+import { useRef, useState } from "react";
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const menuButtonRef = useRef(null);
+
+  const handleOpenMenu = () => setIsMenuOpen(true);
+  const handleCloseMenu = () => setIsMenuOpen(false);
   return (
-    <header className="header">
-      <div className="header__container">
-        <a href="/">
-          <img
-            src="https://chieuphimquocgia.com.vn/images/logo-text-new.svg"
-            alt="Ticket Hub"
-            className="header__logo"
-          />
-        </a>
-        <div>
-          <ul className="header__nav-list">
-            <li>
-              <a href="/" className="header__nav-link">
-                Trang chủ
-              </a>
-            </li>
-            <li>
-              <a href="/movies" className="header__nav-link">
-                Lịch chiếu
-              </a>
-            </li>
-            <li>
-              <a href="/promotions" className="header__nav-link">
-                Khuyến mãi
-              </a>
-            </li>
-            <li>
-              <a href="/faqs" className="header__nav-link">
-                Hỏi đáp
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div className="header__buttons">
-          <MyButton
-            sx={{
-              borderRadius: "20px",
-              backgroundColor: "#FFD700",
-              color: "black",
-              fontWeight: "bold",
-              "&:hover": {
-                backgroundColor: "#FFC000",
-              },
-              marginRight: "35px",
-            }}
-          >
-            Đăng nhập
-          </MyButton>
-          <MyButton
-            variant="outlined"
-            sx={{
-              borderRadius: "20px",
-              borderColor: "white",
-              color: "white",
-              fontWeight: "bold",
-              "&:hover": {
-                backgroundColor: "rgba(255, 255, 255, 0.1)",
+    <Container>
+      <header className="header">
+        <div className="header__container">
+          <a href="/">
+            <img
+              src="https://chieuphimquocgia.com.vn/images/logo-text-new.svg"
+              alt="Ticket Hub"
+              className="header__logo"
+            />
+          </a>
+          <div>
+            <ul className="header__nav-list">
+              <li>
+                <a href="/" className="header__nav-link">
+                  Trang chủ
+                </a>
+              </li>
+              <li>
+                <a href="/movies" className="header__nav-link">
+                  Lịch chiếu
+                </a>
+              </li>
+              <li>
+                <a href="/promotions" className="header__nav-link">
+                  Khuyến mãi
+                </a>
+              </li>
+              <li>
+                <a href="/faqs" className="header__nav-link">
+                  Hỏi đáp
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="header__buttons">
+            <MyButton
+              sx={{
+                borderRadius: "20px",
+                backgroundColor: "#FFD700",
+                color: "black",
+                fontWeight: "bold",
+                "&:hover": {
+                  backgroundColor: "#FFC000",
+                },
+                marginRight: "35px",
+              }}
+            >
+              Đăng nhập
+            </MyButton>
+            <MyButton
+              variant="outlined"
+              sx={{
+                borderRadius: "20px",
                 borderColor: "white",
-              },
-            }}
-          >
-            Đăng ký
-          </MyButton>
+                color: "white",
+                fontWeight: "bold",
+                "&:hover": {
+                  backgroundColor: "rgba(255, 255, 255, 0.1)",
+                  borderColor: "white",
+                },
+              }}
+            >
+              Đăng ký
+            </MyButton>
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+      <header className="header1">
+        <div className="header1__container">
+          <a href="/">
+            <img
+              src="https://chieuphimquocgia.com.vn/images/logo-text-new.svg"
+              alt="Ticket Hub"
+              className="header1__logo"
+            />
+          </a>
+          <h1 className="header1__text">Ticket Hub</h1>
+        </div>
+        <div className="header1__iconContainer">
+          <MyButton ref={menuButtonRef} onClick={handleOpenMenu}>
+            <img src="./ic_menu.svg" alt="a" />
+          </MyButton>
+          <Menu
+            anchorEl={menuButtonRef.current}
+            open={isMenuOpen}
+            onClose={handleCloseMenu}
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "center",
+            }}
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "center",
+            }}
+            className="header1__menuContainer"
+          >
+            <div className="header1__popupContainer">
+              <MenuItem>
+                <a href="/" className="header__nav-link">
+                  Trang chủ
+                </a>
+              </MenuItem>
+              <MenuItem>
+                <a href="/movies" className="header__nav-link">
+                  Lịch chiếu
+                </a>
+              </MenuItem>
+              <MenuItem>
+                <a href="/promotions" className="header__nav-link">
+                  Khuyến mãi
+                </a>
+              </MenuItem>
+              <MenuItem>
+                <a href="/faqs" className="header__nav-link">
+                  Hỏi đáp
+                </a>
+              </MenuItem>
+              <MenuItem>
+                <div className="header__buttons">
+                  <MyButton
+                    sx={{
+                      borderRadius: "20px",
+                      backgroundColor: "#FFD700",
+                      color: "black",
+                      fontWeight: "bold",
+                      "&:hover": {
+                        backgroundColor: "#FFC000",
+                      },
+                      marginRight: "35px",
+                    }}
+                  >
+                    Đăng nhập
+                  </MyButton>
+                  <MyButton
+                    variant="outlined"
+                    sx={{
+                      borderRadius: "20px",
+                      borderColor: "white",
+                      color: "white",
+                      fontWeight: "bold",
+                      "&:hover": {
+                        backgroundColor: "rgba(255, 255, 255, 0.1)",
+                        borderColor: "white",
+                      },
+                    }}
+                  >
+                    Đăng ký
+                  </MyButton>
+                </div>
+              </MenuItem>
+            </div>
+          </Menu>
+        </div>
+      </header>
+    </Container>
   );
 };
 
